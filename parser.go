9f6f00753765d8fdc4126d4c2fd5cff4a05fff25
Rafael Ibraim <ibraim.gm@gmail.com>
2019-08-26 12:07:49 -0300

    Added linter configuration

--- parser.go
+++ parser.go
@@ -15,16 +15,22 @@ type cfgEntry struct {
 	valuePtr     interface{}
 }
 
+// CfgParser is a parser that can load configurations from the command line or the environment
+// variables.
 type CfgParser struct {
 	entries []*cfgEntry
 }
 
+// NewParser returns a new CfgParser, ready to be used.
 func NewParser() *CfgParser {
 	return &CfgParser{
 		entries: make([]*cfgEntry, 0),
 	}
 }
 
+// ParseArgs parsers the arguments in args and load the configuration
+// values according to the settings of the current parser.
+// Note that args must not include the program name
 func (cfg *CfgParser) ParseArgs(args []string) error {
 	for i := 0; i < len(args); i++ {
 		arg := args[i]
@@ -88,18 +94,18 @@ func (cfg *CfgParser) ParseArgs(args []string) error {
 
 		switch v := entry.valuePtr.(type) {
 		case *bool:
-			if bval, err := strconv.ParseBool(val); err != nil {
+			bval, err := strconv.ParseBool(val)
+			if err != nil {
 				return fmt.Errorf("'%v' is not a valid boolean value for argument '%s'", val, arg)
-			} else {
-				*v = bval
 			}
+			*v = bval
 
 		case *int:
-			if ival, err := strconv.ParseInt(val, 10, 64); err != nil {
+			ival, err := strconv.ParseInt(val, 10, 64)
+			if err != nil {
 				return fmt.Errorf("'%v' is not a valid int value for argument '%s'", val, arg)
-			} else {
-				*v = int(ival)
 			}
+			*v = int(ival)
 
 		case *string:
 			*v = val
@@ -112,18 +118,24 @@ func (cfg *CfgParser) ParseArgs(args []string) error {
 	return nil
 }
 
+// OptBool creates a new parser setting to load a bool value from the command line only.
+// After parsing, the value will be available on the returned pointer.
 func (cfg *CfgParser) OptBool(long, short string, defaultValue bool, help string) *bool {
 	val := new(bool)
 	cfg.entries = append(cfg.entries, &cfgEntry{long: long, short: short, help: help, defaultValue: defaultValue, valuePtr: val, isBool: true})
 	return val
 }
 
+// OptInt creates a new parser setting to load a int value from the command line only
+// After parsing, the value will be available on the returned pointer.
 func (cfg *CfgParser) OptInt(long, short string, defaultValue int, help string) *int {
 	val := new(int)
 	cfg.entries = append(cfg.entries, &cfgEntry{long: long, short: short, help: help, defaultValue: defaultValue, valuePtr: val})
 	return val
 }
 
+// OptString creates a new parser setting to load a string value from the command line only
+// After parsing, the value will be available on the returned pointer.
 func (cfg *CfgParser) OptString(long, short, defaultValue, help string) *string {
 	val := new(string)
 	cfg.entries = append(cfg.entries, &cfgEntry{long: long, short: short, help: help, defaultValue: defaultValue, valuePtr: val})
